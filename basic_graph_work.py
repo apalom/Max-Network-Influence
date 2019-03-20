440d1bfd1732e0ec2be478f9682db625f0a60cca
apalom <35828580+apalom@users.noreply.github.com>
2019-03-20 13:44:10 -0600

    Add df['willingness']

--- basic_graph_work.py
+++ basic_graph_work.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 # https://python-graph-gallery.com/320-basic-network-from-pandas-data-frame/
 
 # Load Network Data
-df = pd.read_csv('ca_sandia_auth.csv', header=0);
+df = pd.read_csv('ca_sandia_auth.csv', header=0); #ca_sandia_auth.csv is directed
 df = df.sort_values(by = ['from', 'to'])
 df.reset_index(drop=True, inplace=True)
  
@@ -25,10 +25,10 @@ df.reset_index(drop=True, inplace=True)
 #G=nx.from_pandas_dataframe(df_SandiaAuth, 'from', 'to', create_using=nx.DiGraph() )
 
 # Build your graph
-G = nx.from_pandas_edgelist(df, 'from', 'to') 
+G = nx.from_pandas_edgelist(df, 'from', 'to', create_using=nx.DiGraph()) 
 
 # Plot it
-plt.figure(3,figsize=(12,12)) 
+plt.figure(3,figsize=(8,8)) 
 #nx.draw(G, with_labels=True, node_size=5, node_color="skyblue", alpha=0.5, linewidths=40)
 #nx.draw(G, with_labels=True, node_size=1500, node_color="skyblue", node_shape="o", alpha=0.5, linewidths=4, font_size=25, font_color="grey", font_weight="bold", width=2, edge_color="grey")
 #nx.draw(G, with_labels=True, width=2, node_color="skyblue", style="solid")
@@ -39,32 +39,53 @@ plt.title("Sandia Authorship Network")
 plt.show()
 
 #%% Graph Node Heuristics
-#https://networkx.github.io/documentation/stable/reference/algorithms/index.html
 
-heuristics = ['betweeness', 'closeness', 'degree', 'min dominant']
+# https://networkx.github.io/documentation/stable/reference/algorithms/index.html
+
+heuristics = ['betweeness_cent', 'closeness_cent', 'degree_cent', 'in_degree_cent', 'out_degree_cent', 'min dominant']
 
 bc = nx.betweenness_centrality(G);
-cc = nx.closeness_centrality(G);
+cc = nx.closeness_centrality(G); # same as "distance centrality"
 dc = nx.degree_centrality(G);
-mwds = list(min_weighted_dominating_set(G, weight=None))
-dct = dict(zip(np.arange(0,df.values.max(),1), [0]*df.values.max()))
+# --- networkx.algorithms.centrality.degree_centrality --- #
+#The degree centrality values are normalized by dividing by the maximum possible degree in a simple graph n-1 where n is the number of nodes in G.
+#For multigraphs or graphs with self loops the maximum degree might be higher than n-1 and values of degree centrality greater than 1 are possible.
+in_dc = nx.in_degree_centrality(G);
+out_dc = nx.out_degree_centrality(G);
 
-for m in mwds:
-    dct[m] = 1;
-mwds = dct;
+# --- Min Weight Dominating set only for undirected graph --- #
+#This algorithm computes an approximate minimum weighted dominating set for the graph G. The upper-bound on the size of the solution is log w(V) * OPT. Runtime of the algorithm is \(O(|E|)\).
+#References: Vazirani, Vijay Approximation Algorithms (2001)
+
+#mwds = list(min_weighted_dominating_set(G, weight=None))
+#dct = dict(zip(np.arange(0,df.values.max(),1), [0]*df.values.max()))
+#
+#for m in mwds:
+#    dct[m] = 1;
+#mwds = dct;
 
 #networkx.algorithms.approximation.dominating_set.min_weighted_dominating_set
 
 df_nodes = pd.DataFrame(data=None, index=np.arange(0,df.values.max(),1), columns=heuristics)
 
-df_nodes['betweeness'] = pd.DataFrame.from_dict(bc, orient='index')
-df_nodes['closeness'] = pd.DataFrame.from_dict(cc, orient='index')
-df_nodes['degree'] = pd.DataFrame.from_dict(dc, orient='index')
-df_nodes['min dominant'] = pd.DataFrame.from_dict(mwds  , orient='index')
+df_nodes['betweeness_cent'] = pd.DataFrame.from_dict(bc, orient='index')
+df_nodes['closeness_cent'] = pd.DataFrame.from_dict(cc, orient='index')
+df_nodes['degree_cent'] = pd.DataFrame.from_dict(dc, orient='index')
+df_nodes['in_degree_cent'] = pd.DataFrame.from_dict(in_dc, orient='index')
+df_nodes['out_degree_cent'] = pd.DataFrame.from_dict(out_dc, orient='index')
+#df_nodes['min dominant'] = pd.DataFrame.from_dict(mwds  , orient='index')
 
-#%% Graph Edge Heuristics
+#%% Seed Nodes
 
-heuristics = ['betweeness', 'closeness', 'degree', 'min dominant']
+import random
 
-df_edges = df;
+# Assign likelihood of adoption for all vertices
+df['willingness'] = df['willingness'].apply(lambda x: random.random())
 
+# Assign Seed Vertices 
+n = 1
+seeds = random.sample(set(np.arange(0,len(df),1)), n)
+print('Seeds: ', seeds)
+
+for v in seeds:
+    print(v)
